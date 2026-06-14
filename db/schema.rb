22aee3ed6a7e6a2979c4f268304919a226518617
tadrs <tadrosgamal992@gmail.com>
2026-06-14 23:52:51 +0300

    make post scaffold and make relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_14_204848) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_14_205027) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
